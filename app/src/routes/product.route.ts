55dbf0c0d44c84712816e3ff675ae7cd295d3a32
WolfY <david.skliarov.dev@gmail.com>
2026-05-18 13:08:52 +0400

    feat: add rootPath variable

--- app/src/routes/product.route.ts
+++ app/src/routes/product.route.ts
@@ -1,14 +1,11 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import express from "express";
-
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__fileName);
+import { rootPath } from "../utils/path";
 
 const router = express.Router();
 
 router.get("/add-product", (_, res) => {
-	res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+	res.sendFile(path.join(rootPath, "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res) => {
--- app/src/routes/shop.route.ts
+++ app/src/routes/shop.route.ts
@@ -1,14 +1,11 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import express from "express";
-
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__fileName);
+import { rootPath } from "../utils/path";
 
 const router = express.Router();
 
 router.get("/", (_, res) => {
-	res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+	res.sendFile(path.join(rootPath, "views", "shop.html"));
 });
 
 export default router;
