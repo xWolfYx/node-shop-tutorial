5b3552bec99284a5e0189a9ce3c12543fed7f94c
WolfY <david.skliarov.dev@gmail.com>
2026-05-28 16:36:29 +0400

    feat: add admin products route

--- app/src/routes/admin.route.ts
+++ app/src/routes/admin.route.ts
@@ -2,11 +2,13 @@ import express from "express";
 import {
 	addProduct,
 	renderAddProducts,
+	renderAdminProducts,
 } from "../controllers/products.controller";
 
 const router = express.Router();
 
 router.get("/add-product", renderAddProducts);
+router.get("/products", renderAdminProducts);
 
 router.post("/add-product", addProduct);
 
