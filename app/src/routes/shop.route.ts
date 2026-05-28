f0fe67c1c69846396d677d7fcf26eea933911bb3
WolfY <david.skliarov.dev@gmail.com>
2026-05-28 16:39:45 +0400

    feat: add cart rendering route

--- app/src/routes/shop.route.ts
+++ app/src/routes/shop.route.ts
@@ -3,7 +3,9 @@ import express from "express";
 const router = express.Router();
 
 import { renderProducts } from "../controllers/products.controller";
+import { renderCart } from "../controllers/shop.controller";
 
 router.get("/", renderProducts);
+router.get("/cart", renderCart);
 
 export default router;
