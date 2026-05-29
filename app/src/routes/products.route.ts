74bc17981d09aeca9038725e48c93b81c7e3124c
WolfY <david.skliarov.dev@gmail.com>
2026-05-29 14:17:18 +0400

    feat: add products rendering function for the products route

--- app/src/routes/products.route.ts
+++ app/src/routes/products.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { addProduct } from "../controllers/products.controller";
+import { addProduct, renderProducts } from "../controllers/products.controller";
 
 const router = express.Router();
 
+router.get("/products", renderProducts);
 router.post("/add-product", addProduct);
 
 export { router };
