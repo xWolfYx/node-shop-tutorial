76e429756f59c6313cf37d3d048effb689b25e22
WolfY <david.skliarov.dev@gmail.com>
2026-05-29 14:18:03 +0400

    feat: add rendering functions for index and checkout routes

--- app/src/controllers/shop.controller.ts
+++ app/src/controllers/shop.controller.ts
@@ -1,5 +1,15 @@
 import type { Request, Response } from "express";
 
+export const renderIndex = async (_: Request, res: Response) => {
+	res.render("shop/index", {
+		pageTitle: "Shop",
+	});
+};
+
 export const renderCart = (_: Request, res: Response) => {
 	res.render("shop/cart", { pageTitle: "Cart" });
+};
+
+export const renderCheckout = (_: Request, res: Response) => {
+	res.render("shop/checkout", { pageTitle: "Checkout" });
 };
