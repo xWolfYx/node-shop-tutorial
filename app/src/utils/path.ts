960e539e95899c224400968677abc9d9011b3df6
WolfY <david.skliarov.dev@gmail.com>
2026-06-02 15:39:39 +0400

    feat: add findById function

--- app/src/utils/path.ts
+++ app/src/utils/path.ts
@@ -1,7 +1,12 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import type { Request } from "express";
+import type { ProductData } from "../models/product";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const rootPath = path.join(__dirname, "..");
+
+export const findById = (req: Request, p: ProductData) =>
+	p.id === req.params.id;
