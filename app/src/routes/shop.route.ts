8b0f99a8fc22bcee03a0ebba262a54d60d1e6178
WolfY <david.skliarov.dev@gmail.com>
2026-06-02 16:57:46 +0400

    feat: add post request to the /cart

--- app/src/routes/shop.route.ts
+++ app/src/routes/shop.route.ts
@@ -10,7 +10,13 @@ import {
 } from "../controllers/shop.controller";
 
 router.get("/", renderIndex);
+
 router.get("/cart", renderCart);
+router.post("/cart", (req, res, next) => {
+	console.log(req.body);
+	next();
+});
+
 router.get("/orders", renderOrders);
 router.get("/checkout", renderCheckout);
 
