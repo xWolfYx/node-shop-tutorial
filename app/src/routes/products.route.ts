4c3184d596e2fdc964d85839f4aba1cda56ca6b4
WolfY <david.skliarov.dev@gmail.com>
2026-06-02 16:56:42 +0400

    feat: implement product detail view route

--- app/src/routes/products.route.ts
+++ app/src/routes/products.route.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import { addProduct, renderProducts } from "../controllers/products.controller";
+import {
+	addProduct,
+	renderProduct,
+	renderProducts,
+} from "../controllers/products.controller";
 
 const router = express.Router();
 
 router.get("/products", renderProducts);
+router.get("/products/:id", renderProduct);
 router.post("/add-product", addProduct);
 
 export { router };
