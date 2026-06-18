7595b41d04a569f7d10c5118aee6911e1e7cf0bf
WolfY <david.skliarov.dev@gmail.com>
2026-06-18 13:11:34 +0400

    refactor: change commonjs __dirname code to esModule

--- app/src/utils/path.ts
+++ app/src/utils/path.ts
@@ -1,10 +1,8 @@
 import path from "node:path";
-import { fileURLToPath } from "node:url";
 import type { Request } from "express";
 import type { ProductData } from "../models/product";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = import.meta.dirname;
 
 export const rootPath = path.join(__dirname, "..");
 
