8c5c5e92d284871fe2fcfd76830f02c1477b22d0
WolfY <david.skliarov.dev@gmail.com>
2026-06-30 15:58:28 +0400

    refactor: export addToCart function from class

--- app/src/models/cart.ts
+++ app/src/models/cart.ts
@@ -1,40 +1,41 @@
 import fs from "node:fs/promises";
 import path from "node:path";
-import type { CartData, CartProduct, ICart } from "../lib/types.js";
+import type { CartData, CartProduct } from "../lib/types.js";
 import { rootPath } from "../utils/path.js";
 
 const cartFilePath = path.join(rootPath, "data", "cart.json");
 
-export class Cart implements ICart {
-	async addToCart(id: string, productPrice: number) {
-		try {
-			const cartItems = await fs.readFile(cartFilePath, "utf-8");
-			let cart: CartData = { products: [], totalPrice: 0 };
+export async function addToCart(id: string, productPrice: number) {
+	let cart: CartData = { products: [], totalPrice: 0 };
 
-			if (!cartItems.trim()) cart = JSON.parse(cartItems);
-			cart = JSON.parse(cartItems);
+	try {
+		const cartItems = await fs.readFile(cartFilePath, "utf8");
+		if (cartItems.trim()) cart = JSON.parse(cartItems);
+	} catch (err) {
+		console.log(err);
+	}
 
-			const existingProductIndex = cart.products.findIndex((p) => p.id === id);
-			const existingProduct = cart.products[existingProductIndex];
-			let updatedProduct: CartProduct;
+	try {
+		const existingProductIndex = cart.products.findIndex((p) => p.id === id);
+		const existingProduct = cart.products[existingProductIndex];
+		let updatedProduct: CartProduct;
 
-			if (existingProduct) {
-				updatedProduct = {
-					...existingProduct,
-					quantity: existingProduct.quantity + 1,
-				};
-				cart.products = [...cart.products];
-				cart.products[existingProductIndex] = updatedProduct;
-			} else {
-				updatedProduct = { id, quantity: 1 };
-				cart.products = [...cart.products, updatedProduct];
-			}
-			cart.totalPrice += productPrice;
-
-			await fs.writeFile(cartFilePath, JSON.stringify(cart, null, 2));
-		} catch (err) {
-			console.log(err);
+		if (existingProduct) {
+			updatedProduct = {
+				...existingProduct,
+				quantity: existingProduct.quantity + 1,
+			};
+			cart.products[existingProductIndex] = updatedProduct;
+		} else {
+			updatedProduct = { id, quantity: 1 };
+			cart.products = [...cart.products, updatedProduct];
 		}
+		cart.totalPrice += productPrice;
+
+		await fs.writeFile(cartFilePath, JSON.stringify(cart, null, 2));
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 export async function removeFromCart(id: string, productPrice: number) {
