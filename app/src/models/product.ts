7c251b1faaf5efe2118395b7484cee1519a5fab4
WolfY <david.skliarov.dev@gmail.com>
2026-07-03 14:48:37 +0400

    refactor: implement product model with sequelize

--- app/src/models/product.ts
+++ app/src/models/product.ts
@@ -1,42 +1,17 @@
-import { randomUUID } from "node:crypto";
-import fs from "node:fs/promises";
-import path from "node:path";
-import type { ProductData } from "../lib/types.js";
-import db from "../utils/db.js";
-import { rootPath } from "../utils/path.js";
+import { DataTypes } from "sequelize";
+import sequelize from "../utils/db.js";
 
-const productFilePath = path.join(rootPath, "data", "products.json");
+const product = sequelize.define("product", {
+	id: {
+		type: DataTypes.UUID,
+		allowNull: false,
+		primaryKey: true,
+		unique: true,
+	},
+	title: { type: DataTypes.STRING, allowNull: false },
+	imageUrl: { type: DataTypes.STRING, allowNull: false },
+	description: { type: DataTypes.STRING, allowNull: false },
+	price: { type: DataTypes.INTEGER, allowNull: false },
+});
 
-const getData = async (): Promise<ProductData[]> => {
-	try {
-		const data = JSON.parse(await fs.readFile(productFilePath, "utf-8"));
-		return data;
-	} catch (err) {
-		console.log(err);
-		return [];
-	}
-};
-
-export class Product implements ProductData {
-	constructor(
-		public id: string,
-		public title: string,
-		public imageUrl: string,
-		public description: string,
-		public price: number,
-	) {}
-
-	static async fetchAll(): Promise<ProductData[]> {
-		const [rows] = await db.execute(`SELECT * FROM products`);
-		return rows as ProductData[];
-	}
-
-	static async delete(id: string): Promise<void> {
-		const products = await getData();
-		const updatedProducts = products.filter((p) => p.id !== id);
-		await fs.writeFile(
-			productFilePath,
-			JSON.stringify(updatedProducts, null, 2),
-		);
-	}
-}
+export default product;
