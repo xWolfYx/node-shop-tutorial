c46d614b0cdfe77f73912b48ddb152c4aa6e39a0
WolfY <david.skliarov.dev@gmail.com>
2026-07-03 14:54:18 +0400

    fix: import missing toUSD function

--- app/src/controllers/products.controller.ts
+++ app/src/controllers/products.controller.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from "node:crypto";
 import type { Request, Response } from "express";
+import { toUSD } from "../lib/utils.js";
 import Product from "../models/product.js";
 
 export const renderProducts = async (_: Request, res: Response) => {
