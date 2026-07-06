44789582d947b8551ed01aabd4d795fc669aa574
WolfY <david.skliarov.dev@gmail.com>
2026-07-06 17:00:06 +0400

    feat: add cart-item model

---
+++ app/src/models/cart-item.ts
@@ -0,0 +1,14 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../utils/db.js";
+
+const cartItem = sequelize.define("cartItem", {
+	id: {
+		type: DataTypes.UUID,
+		primaryKey: true,
+		allowNull: false,
+	},
+
+	quantity: DataTypes.INTEGER,
+});
+
+export default cartItem;
