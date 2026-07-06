f506d4a2fd16b2a540d04ab59f7a3249e862482a
WolfY <david.skliarov.dev@gmail.com>
2026-07-06 17:02:25 +0400

    feat: add relations to users, products and carts

--- app/src/app.ts
+++ app/src/app.ts
@@ -5,6 +5,13 @@ import { renderNotfoundPage } from "./controllers/notfound.controller.js";
 import { router as adminRouter } from "./routes/admin.route.js";
 import { router as productRouter } from "./routes/products.route.js";
 import shopRouter from "./routes/shop.route.js";
+import "./models/product.js";
+import { randomUUID } from "node:crypto";
+import Cart from "./models/cart.js";
+import CartItem from "./models/cart-item.js";
+import Product from "./models/product.js";
+import User from "./models/user.js";
+import sequelize from "./utils/db.js";
 import { rootPath } from "./utils/path.js";
 
 const PORT = 3000;
@@ -29,6 +36,21 @@ app.use(productRouter);
 
 app.use(renderNotfoundPage);
 
-app.listen(PORT, () => {
-	console.log(`App is running on http://localhost:${PORT}`);
-});
+try {
+	User.hasMany(Product);
+	Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+
+	User.hasOne(Cart, { constraints: true, onDelete: "CASCADE" });
+	Cart.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+
+	Cart.belongsToMany(Product, { through: CartItem });
+	Product.belongsToMany(Cart, { through: CartItem });
+	sequelize.sync();
+
+
+	app.listen(PORT, () =>
+		console.log(`App is running on http://localhost:${PORT}`),
+	);
+} catch (err) {
+	console.log(err);
+}
