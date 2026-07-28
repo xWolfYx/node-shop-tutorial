44ee0960497533a7f3cdb4ef3c30411b9fc35567
WolfY <david.skliarov.dev@gmail.com>
2026-07-28 16:54:58 +0400

    refactor: remove redundant imports and sequelize queries

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,9 +1,5 @@
 import path from "node:path";
-import express, {
-	type NextFunction,
-	type Request,
-	type Response,
-} from "express";
+import express from "express";
 import { renderNotfoundPage } from "./controllers/notfound.controller.js";
 import adminRouter from "./routes/admin.route.js";
 import orderRouter from "./routes/order.route.js";
@@ -11,17 +7,9 @@ import productRouter from "./routes/products.route.js";
 import shopRouter from "./routes/shop.route.js";
 import "./models/product.js";
 import cartRouter from "../src/routes/cart.route.js";
-import Cart from "./models/cart.js";
-import CartItem from "./models/cart-item.js";
-import Order from "./models/order.js";
-import OrderItem from "./models/order-item.js";
-import Product from "./models/product.js";
-import User from "./models/user.js";
-import sequelize from "./utils/db.js";
 import { rootPath } from "./utils/path.js";
 
 const PORT = 8000;
-const DUMMY_USER_ID = "a6296334-b56d-465d-a7bf-6da234b9e0a7";
 
 const app = express();
 
@@ -36,16 +24,6 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use(async (req: Request, res: Response, next: NextFunction) => {
-	try {
-		(req as any).user = await User.findByPk(DUMMY_USER_ID);
-
-		next();
-	} catch (err) {
-		next(err);
-	}
-});
-
 app.use("/admin", adminRouter);
 app.use(cartRouter);
 app.use(orderRouter);
@@ -54,42 +32,6 @@ app.use(shopRouter);
 
 app.use(renderNotfoundPage);
 
-try {
-	User.hasMany(Product);
-	Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-
-	User.hasOne(Cart, { constraints: true, onDelete: "CASCADE" });
-	Cart.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-
-	Cart.belongsToMany(Product, { through: CartItem });
-	Product.belongsToMany(Cart, { through: CartItem });
-
-	User.hasMany(Order, { constraints: true, onDelete: "CASCADE" });
-	Order.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-	Order.belongsToMany(Product, { through: OrderItem });
-	Product.belongsToMany(Order, { through: OrderItem });
-
-	await sequelize.sync();
-
-	let user = await User.findByPk(DUMMY_USER_ID);
-
-	if (!user) {
-		user = await User.create({
-			id: DUMMY_USER_ID,
-			name: "David",
-			email: "david@email.com",
-		});
-	}
-
-	const cart = await user.getCart();
-
-	if (!cart) {
-		await user.createCart();
-	}
-
-	app.listen(PORT, () =>
-		console.log(`App is running on http://localhost:${PORT}`),
-	);
-} catch (err) {
-	console.log(err);
-}
+app.listen(PORT, () =>
+	console.log(`App is running on http://localhost:${PORT}`),
+);
