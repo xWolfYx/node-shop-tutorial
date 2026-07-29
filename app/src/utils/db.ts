252c926be07c38f78ebc9e6827e5309a0e8da68a
WolfY <david.skliarov.dev@gmail.com>
2026-07-29 16:45:36 +0400

    feat: implement MongoDB connection script

--- app/src/utils/db.ts
+++ app/src/utils/db.ts
@@ -1,8 +1,34 @@
-import { Sequelize } from "sequelize";
+import { type Db, MongoClient } from "mongodb";
 
-const sequelize = new Sequelize("product-shop", "root", "StrongPassword", {
-	dialect: "mysql",
-	host: "localhost",
-});
+const { MONGO_URI } = process.env;
 
-export default sequelize;
+if (!MONGO_URI) {
+	throw new Error("Please define the MONGO_URI environmental variable");
+}
+
+const client = new MongoClient(MONGO_URI);
+
+let db: Db | null = null;
+
+const connectDB = async (): Promise<Db> => {
+	if (db) return db;
+
+	try {
+		await client.connect();
+		console.log("Connected successfully to MongoDB");
+
+		db = client.db("product-shop");
+		return db;
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+};
+
+const getDB = () => {
+	if (!db) throw new Error("Database not initialized. Call connectDB first.");
+
+	return db;
+};
+
+export { connectDB, getDB };
