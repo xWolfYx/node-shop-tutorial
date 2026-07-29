99d13dfaaded91569a6e201d8982d066a5bb32db
WolfY <david.skliarov.dev@gmail.com>
2026-07-29 16:52:36 +0400

    feat: implement fetchAllProducts, fetchProduct and save functions

---
+++ app/src/models/product.ts
@@ -0,0 +1,57 @@
+import { ObjectId } from "mongodb";
+import { getDB } from "../utils/db.js";
+
+export default class Product {
+	title: string;
+	imageUrl: string;
+	description: string;
+	price: number;
+
+	constructor(
+		title: string,
+		imageUrl: string,
+		description: string,
+		price: number,
+	) {
+		this.title = title;
+		this.imageUrl = imageUrl;
+		this.description = description;
+		this.price = price;
+	}
+
+	async save() {
+		const db = getDB();
+
+		try {
+			return await db.collection("products").insertOne(this);
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	static async fetchAllProducts() {
+		const db = getDB();
+
+		try {
+			const result = await db.collection("products").find().toArray();
+			return result;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	static async fetchProduct(id: string) {
+		if (!ObjectId.isValid(id)) return null;
+
+		const db = getDB();
+
+		try {
+			const result = await db
+				.collection("products")
+				.findOne({ _id: new ObjectId(id) });
+			return result;
+		} catch (err) {
+			console.log(err);
+		}
+	}
+}
